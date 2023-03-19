ccd7e60065d259fbe207d89e17f820a9cbd86d53
Gavin Dmello <gavinrocks1997@gmail.com>
2023-03-19 18:34:30 -0400

    added experience card segment. added custom icon onclick action with redirect option to company website.

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -12,6 +12,58 @@ import { experiences } from "../constants";
 import SectionWrapper from "../hoc/SectionWrapper";
 import { textVariant } from "../utils/motion";
 
+const ExperienceCard = ({ experience }) => {
+  return (
+    <VerticalTimelineElement
+      contentStyle={{
+        background: "#1d1836",
+        color: "#fff",
+      }}
+      contentArrowStyle={{ borderRight: "7px solid  #232631" }}
+      date={experience.date}
+      iconStyle={{
+        background: experience.iconBg,
+      }}
+      icon={
+        <div className="vte-icon-container position-relative flex justify-center items-center w-full h-full">
+          <img
+            src={experience.icon}
+            alt={experience.company_name}
+            className="w-[70%] h-[70%] object-contain main-img"
+          />
+          <img
+            src={open}
+            alt={experience.company_name}
+            className="w-[40%] h-[40%] object-contain hover-img"
+          />
+        </div>
+      }
+      iconOnClick={() => window.open(experience.url, "Experience")}
+    >
+      <div>
+        <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+        <p
+          className="text-secondary text-[16px] font-semibold"
+          style={{ margin: 0 }}
+        >
+          {experience.company_name}
+        </p>
+      </div>
+
+      <ul className="mt-5 list-disc ml-5 space-y-2">
+        {experience.points.map((point, index) => (
+          <li
+            key={`experience-point-${index}`}
+            className="text-white-100 text-[14px] pl-1 tracking-wider"
+          >
+            {point}
+          </li>
+        ))}
+      </ul>
+    </VerticalTimelineElement>
+  );
+};
+
 const Experience = () => {
   return (
     <>
@@ -27,7 +79,10 @@ const Experience = () => {
       <div className="mt-20 flex flex-col">
         <VerticalTimeline>
           {experiences.map((experience, index) => (
-            <></>
+            <ExperienceCard
+              key={`experience-${index}`}
+              experience={experience}
+            />
           ))}
         </VerticalTimeline>
       </div>
